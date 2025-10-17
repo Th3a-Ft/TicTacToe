09a8e8330055530d42e95a8a03cd5984659c5c56
Th3a-Ft <thea.fort@le-campus-numerique.fr>
2025-10-17 16:39:55 +0200

    update condition of victory (method check diagonal)

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -13,6 +13,7 @@ public class TicTacToe {
 
     /**
      * Constructor of the class TicTacToe
+     *
      * @param size = size of a side of the board (the board is a square so the number of cell equal size*size)
      */
     TicTacToe(int size) {
@@ -22,6 +23,7 @@ public class TicTacToe {
 
     /**
      * Create the 2D array to create the board
+     *
      * @param size = size of a side of the board
      * @return an array of an array (2D array)
      */
@@ -40,6 +42,7 @@ public class TicTacToe {
 
     /**
      * Create a 1D array
+     *
      * @param size = size of the board
      */
     public void initBoard1D(int size) {
@@ -64,6 +67,7 @@ public class TicTacToe {
 
     /**
      * The player enter an int for the row and for the col and check if the Cell is empty or not
+     *
      * @return the coordinates enter by the player (x,y)
      */
     private ArrayList<Integer> getMoveFromPlayer() {
@@ -99,6 +103,7 @@ public class TicTacToe {
 
     /**
      * Check that the value enter by the user to choose a row and a col are valid
+     *
      * @param coordinate = input by the player
      * @return the correct input
      */
@@ -118,8 +123,9 @@ public class TicTacToe {
 
     /**
      * Give the ownership of the cell to a player according the cell he played
-     * @param x is the row choose by the player
-     * @param y is the col choose by the player
+     *
+     * @param x      is the row choose by the player
+     * @param y      is the col choose by the player
      * @param player is the player currently playing
      */
     private void setOwner(int x, int y, Player player) {
@@ -147,15 +153,26 @@ public class TicTacToe {
         }
 
         System.out.println(firstPlayer.getName() + " against " + secondPlayer.getName());
-        while (!isFull() || !rowFull()) {
-            System.out.println(currentPlayer.getName() + " it's your turn to play");
+
+        while (!isOver()) {
             getMoveFromPlayer();
-            currentPlayer = (currentPlayer == firstPlayer) ? secondPlayer : firstPlayer;
+            if (isOver()) {
+                System.out.print("Congrats " + firstPlayer.getName() + "! You win this game against " + secondPlayer.getName());
+                System.exit(0);
+            } else
+                if (isFull()) {
+                System.out.print("It's a draw");
+                System.exit(0);
+            } else {
+                System.out.println(currentPlayer.getName() + " it's your turn to play");
+                currentPlayer = (currentPlayer == firstPlayer) ? secondPlayer : firstPlayer;
+            }
         }
     }
 
     /**
      * Create a player with a name, each player has the choice to play X or O
+     *
      * @return the player with his name and his pawn (X or O)
      */
     private Player initPlayer() {
@@ -176,7 +193,8 @@ public class TicTacToe {
                 incorrectChoice = false;
             } else {
                 System.out.println("Invalid input, try again");
-                choicePlayer = input.nextLine();
+                choicePlayer = input.nextLine().toUpperCase();
+
             }
         System.out.println(player.getName() + " " + player.getRepresentation());
         return player;
@@ -184,6 +202,7 @@ public class TicTacToe {
 
     /**
      * Check if the bord is full or not
+     *
      * @return if true the game stops / if false the came continues
      */
     private boolean isFull() {
@@ -200,24 +219,97 @@ public class TicTacToe {
 
     /**
      * Check if a row is full or not with the same representation (x or o)
+     *
      * @return if true stop the game with a winner / if false the game continues
      */
-    private boolean rowFull() {
+    private boolean rowWinner() {
         int x = coordinates.get(0);
-        int count =0;
+        int count = 0;
         for (int i = 0; i < board.size(); ++i) {
-            if (board.get(x).get(i).getRepresentation().equals("   ")) {
-                return false;
-            } else if (board.get(x).get(i).getRepresentation().equals(currentPlayer.getRepresentation())) {
+            if (board.get(x).get(i).getRepresentation().equals(currentPlayer.getRepresentation())) {
                 count++;
                 if (count == board.size()) {
-                    System.out.println("Winner");
+                    return true;
                 }
-
-                //return true;
+            } else {
+                return false;
             }
         }
+        return false;
+    }
 
+    /**
+     * Check if the current player wins by counting the number of his representation (x or o) in the col
+     *
+     * @return true if number is equal to the size of the board and the player win or false if it's not
+     */
+    private boolean colWinner() {
+        int y = coordinates.get(1);
+        int count = 0;
+        for (int i = 0; i < board.size(); ++i) {
+            if (board.get(i).get(y).getRepresentation().equals(currentPlayer.getRepresentation())) {
+                count++;
+                if (count == board.size()) {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Check if the diag going down if fill with the same representation or not
+     *
+     * @return true if the cells are all the same (win for the player with this representation) / false if all the cells are not the same
+     */
+    private boolean diagDownWinner() {
+        int count = 0;
+
+        for (int i = 1; i < board.size(); ++i) {
+            if (board.get(i).get(i).getRepresentation().equals("   ")) {
+                return false;
+            }
+            if (board.get(i).get(i).getRepresentation().equals(board.get(i - 1).get(i - 1).getRepresentation())) {
+                count++;
+                if (count == board.size() - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Check if the diag UP is fill with the same representation
+     *
+     * @return true if the cells are all the same (win for the player with this representation) / false if all the cells are not the same
+     */
+    private boolean diagUpWinner() {
+        int size = board.size();
+        String first = board.get(size - 1).get(0).getRepresentation();
+
+        if (first.equals("   ")) {
+            return false;
+        }
+
+        for (int i = 1; i < size; i++) {
+            String current = board.get(size - 1 - i).get(i).getRepresentation();
+            if (!current.equals(first)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isOver() {
+        if (diagDownWinner() || diagUpWinner() || rowWinner() || colWinner()) {
+            return true;
+        } else if (isFull()) {
+            System.out.println("It's a draw");
+            return true;
+        }
         return false;
     }
 
